43854d0c1625cb6f93b757324877265a7150cd2b
Administrator <Administrator@172.31.15.37>
2021-10-07 09:38:34 +0000

    Updation of NonNull

--- src/main/java/com/flightApp/DAO/Airline.java
+++ src/main/java/com/flightApp/DAO/Airline.java
@@ -4,14 +4,20 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.lang.NonNull;
+
 @Entity
 @Table(name = "airlineDetails")
 public class Airline {
 
 	@Id
+	@NonNull
 	private String name;
+	@NonNull
 	private String address;
+	@NonNull
 	private String contactNumber;
+	
 	public String getName() {
 		return name;
 	}
